1e3eb36a6059fe5437caa1426a4ee18f3b026104
Vincent Robert <vincent.robert@nanego.com>
2018-01-17 16:13:45 +0100

    Add some helper functions

---
+++ assets/javascripts/helper_functions.js
@@ -0,0 +1,167 @@
+function concat(previous_text, new_text){
+  if(exists(new_text)){
+    if(exists(previous_text))
+      previous_text += " ";
+    previous_text += new_text;
+  }
+  return previous_text;
+}
+
+function findByAttribute(array, attr, value) {
+
+  log("00", value );
+
+  if(array != undefined){
+
+    if(Array.isArray(array)){
+      for(let i = 0; i < array.length; i += 1) {
+        if(array[i][attr] === value) {
+          return i;
+        }
+      }
+    }else{
+      for (let key of Object.keys(array)) {
+
+        log("01", array[key] );
+
+        if(array[key][attr] === value) {
+
+          log("02", array[key][attr] );
+
+          return key;
+        }
+      }
+    }
+
+
+  }else{
+    return undefined;
+  }
+}
+
+function log(string, object = undefined){
+  let print = "";
+  if(object){
+    print = string + " : " + JSON.stringify(object);
+  }else{
+    print = string;
+  }
+  console.log(print);
+  return print;
+}
+
+function merge(o1, o2){
+  let merged_object = {};
+  Object.assign(merged_object, o1, o2);
+  return merged_object;
+}
+
+function getNamesFromIds(array, ids){
+  let names = [];
+  if(ids instanceof Array){
+    for (let id of ids) {
+      let name = getNameFromId(array, id);
+      if(name){
+        names.push(name);
+      }
+    }
+  }else{
+    let name = getNameFromId(array, ids);
+    if(name) {
+      names.push(name);
+    }
+  }
+  return names;
+}
+
+function getNameFromId(array, id){
+  let element = array.find(function (d) {
+    return d.id == id;
+  });
+  if(element){
+    return to_s(element);
+  }else{
+    return undefined;
+  }
+}
+
+function getIdFromName(array, name){
+  let element = array.find(function (d) {
+    return to_s(d).toLowerCase() === name.toLowerCase();
+  });
+  if(element){
+    return element.id
+  }else{
+    return undefined
+  }
+}
+
+function isInteger(value){
+   return value==parseInt(value);
+}
+
+function getIdByValue(array, value){
+  if(value==parseInt(value)){
+    // value is an ID
+    return parseInt(value)
+  }else{
+    // value is a string
+    value = value.replace(/"/g, '');
+    return getIdFromName(array, value)
+  }
+}
+
+function surroundWithQuotesIfNecessary(value){
+  log("surroud if necessary", value);
+  if(isString(value) && value.indexOf(' ')>0){
+    return "\"" + value + "\""
+  }else{
+    return value
+  }
+}
+
+function isString(value){
+  return (typeof value === 'string' || value instanceof String);
+}
+
+function exists(value){
+  if( value != undefined && (value > 0 || value.length > 0 || typeof value === "boolean" || typeof value === "object") ){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function removeBlankAttributes(object){
+
+  log("start", object);
+
+  let obj = Object.assign({}, object);
+  Object.keys(obj).forEach(key => ((obj[key]!==false && !obj[key]) || JSON.stringify(obj[key])===JSON.stringify({}) ) && delete obj[key]);
+
+  log("removeBlankAttributes", obj);
+
+  return obj;
+}
+
+function convertToBoolean(value){
+
+  log('convertToBoolean', value);
+
+  if (isString(value)){
+    value = value.toLowerCase();
+  }
+  return (value != 'false' && value != '0' && value != 'no' && value!==false);
+}
+
+function getFilterValue(filter){
+  if(filter){
+    if(filter.value){
+      return filter.value
+    }else{
+      return filter
+    }
+  }else{
+    return "";
+  }
+}
--- lib/redmine_base_stimulusjs/hooks.rb
+++ lib/redmine_base_stimulusjs/hooks.rb
@@ -3,6 +3,7 @@ module RedmineBaseStimulusJS
     def view_layouts_base_html_head(context)
       tags = javascript_include_tag("stimulusjs-0.9.0.js", :plugin => "redmine_base_stimulusjs")
       tags += javascript_tag("var stimulus_application;(function() {stimulus_application = Stimulus.Application.start();})()")
+      tags += javascript_include_tag("helper_functions.js", :plugin => "redmine_base_stimulusjs")
     end
   end
 end
